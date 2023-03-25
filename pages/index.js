f6b627ca82611cfd44c6c6bff835e2b419a12969
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-25 16:45:32 +0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 
-
 export default function Home(){
   return (
     <div>
 
-      <div className="hero">
-        <h1 className="title">Welcome to Next.js!</h1>
-        <p className="description">
-          To get started, edit <code>pages/index.js</code> and save to reload.
-        </p>
-      </div>
+      <h1 className="title">Welcome to Next.js!</h1>
+      <p className="description">
+        To get started, edit <code>pages/index.js</code> and save to reload.
+      </p>
 
       </div>
 )}
