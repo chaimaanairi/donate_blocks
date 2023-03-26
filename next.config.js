209676303932ef26d555f3d93fc5a68d35914a49
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-26 18:05:36 +0300

    commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains:  ['ipfs.infura.io', 'th.bing.com', 'donateblock.infura-ipfs.io']
+   }
 }
 
 module.exports = nextConfig
+
+
