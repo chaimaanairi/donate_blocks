5187875837a47b9f478f3667da33fcaca5a2cddb
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-26 11:51:40 +0300

    commit

--- components/Wallet.js
+++ components/Wallet.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import styled from "styled-components";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 const Wallet = () => {
   return (
@@ -6,6 +9,40 @@ const Wallet = () => {
       Wallet
     </div>
   )
-}
+};
 
-export default Wallet
+const ConnectWalletWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${(props) => props.theme.bgDiv};
+  padding: 5px 9px;
+  height: 100%;
+  color: ${(props) => props.theme.color};
+  border-radius: 10px;
+  margin-right: 15px;
+  font-family: 'Roboto';
+  font-weight: bold;
+  font-size: small;
+  cursor: pointer;
+`;
+
+const Address = styled.h2`
+    background-color: ${(props) => props.theme.bgSubDiv};
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 5px 0 5px;
+    border-radius: 10px;
+`
+
+const Balance = styled.h2`
+    display: flex;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    margin-right: 5px;
+`
+
+export default Wallet;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,116 @@
+import styled from 'styled-components';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import PaidIcon from '@mui/icons-material/Paid';
+import EventIcon from '@mui/icons-material/Event';
+import Image from 'next/image';
+import { ethers } from 'ethers';
+import DonationTracking from '../artifacts/contracts/DonationTracking.sol/DonationTracking.json'
+import { useState } from 'react';
+import Link from 'next/link'
 
-export default function Home(){
+export default function Index({AllData, HealthData, EducationData,AnimalData}) {
+  const [filter, setFilter] = useState(AllData);
+
   return (
+   <HomeWrapper>
+
     <div className="">
 
       <h1 className="">Welcome to Next.js!</h1>
       hello
 
       </div>
-)}
+      
+   </HomeWrapper>
+
+  )
+}
+
+const HomeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
+const FilterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 80%;
+  margin-top: 15px;
+`
+const Category = styled.div`
+  padding: 10px 15px;
+  background-color: ${(props) => props.theme.bgDiv};
+  margin: 0px 15px;
+  border-radius: 8px;
+  font-family: 'Poppins';
+  font-weight: normal;
+  cursor: pointer;
+`
+const CardsWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  width: 80%;
+  margin-top: 25px;
+`
+const Card = styled.div`
+  width: 30%;
+  margin-top: 20px;
+  background-color: ${(props) => props.theme.bgDiv};
+  &:hover{
+    transform: translateY(-10px);
+    transition: transform 0.5s;
+  }
+  
+  &:not(:hover){
+    transition: transform 0.5s;
+  }
+`
+const CardImg = styled.div`
+  position: relative;
+  height: 120px;
+  width: 100%;
+`
+const Title = styled.h2`
+  font-family: 'Roboto';
+  font-size: 18px;
+  margin: 2px 0px;
+  background-color: ${(props) => props.theme.bgSubDiv};
+  padding: 5px;
+  cursor: pointer;
+  font-weight: normal;
+`
+const CardData = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 2px 0px;
+  background-color: ${(props) => props.theme.bgSubDiv};
+  padding: 5px;
+  cursor: pointer;
+  `
+const Text = styled.p`
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto';
+  font-size: 18px;
+  font-weight: bold;
+`
+const Button = styled.button`
+  padding: 8px;
+  text-align: center;
+  width: 100%;
+  background-color:#00b712 ;
+  background-image:
+      linear-gradient(180deg, #00b712 0%, #5aff15 80%); 
+  border: none;
+  cursor: pointer;
+  font-family: 'Roboto';
+  text-transform: uppercase;
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  `
