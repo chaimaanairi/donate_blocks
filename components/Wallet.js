9e0e760ae2699ab87e32824a49e6d330dcf93a81
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-26 13:05:00 +0300

    commit

--- components/Wallet.js
+++ components/Wallet.js
@@ -3,13 +3,54 @@ import styled from "styled-components";
 import { ethers } from "ethers";
 import { useState } from "react";
 
-const Wallet = () => {
-  return (
-    <div>
-      Wallet
-    </div>
-  )
+const networks = {
+  polygon: {
+    chainId: `0x${Number(80001).toString(16)}`,
+    chainName: "Polygon Testnet",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc-mumbai.maticvigil.com/"],
+    blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+  },
 };
+
+
+const Wallet = () => {
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState("");
+
+  const connectWallet = async () => {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    if (provider.network !== "matic") {
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            ...networks["polygon"],
+          },
+        ],
+      });
+    } 
+      const account = provider.getSigner();
+      const Address = await account.getAddress();
+      setAddress(Address);
+      const Balance = ethers.utils.formatEther(await account.getBalance());
+      setBalance(Balance);
+    
+  };
+
+  return (
+    <ConnectWalletWrapper onClick={connectWallet}>
+      {balance == '' ? <Balance></Balance> : <Balance>{balance.slice(0,4)} Matic</Balance> }
+      {address == '' ? <Address>Connect Wallet</Address> : <Address>{address.slice(0,6)}...{address.slice(39)}</Address>}
+    </ConnectWalletWrapper>
+  );
+};
+
 
 const ConnectWalletWrapper = styled.div`
   display: flex;
