b8c3f526fbfe2d3abe11a990378d39b5eb364e13
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-27 00:56:48 +0300

    commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Header from "./header";
+import Footer from "./footer"
 import themes from "./themes";
 import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import { useState, createContext } from "react";
@@ -22,6 +23,7 @@ const Layout = ({ children }) => {
           <GlobalStyle />
           <Header />
           {children}
+          <Footer />
         </LayoutWrapper>
       </ThemeProvider>
     </App.Provider>
