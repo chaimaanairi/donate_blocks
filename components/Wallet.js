6134e930cdb0c37abcad136739cef3214680de4e
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-28 02:18:39 +0300

    commit

--- components/Wallet.js
+++ components/Wallet.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from "styled-components";
 import { ethers } from "ethers";
 import { useState } from "react";
 
@@ -44,44 +43,20 @@ const Wallet = () => {
   };
 
   return (
-    <ConnectWalletWrapper onClick={connectWallet}>
-      {balance == '' ? <Balance></Balance> : <Balance>{balance.slice(0,4)} Matic</Balance> }
-      {address == '' ? <Address>Connect Wallet</Address> : <Address>{address.slice(0,6)}...{address.slice(39)}</Address>}
-    </ConnectWalletWrapper>
+    <div className='bg-white font flex items-center justify-between rounded-[10px] font-bold cursor-pointer' onClick={connectWallet}>
+      {balance == '' ? 
+        <div className='flex items-center h-[100%] justify-center mr-[5px]'></div> : 
+        <div className='flex items-center h-[100%] justify-center mr-[5px]'>{balance.slice(0,4)} Matic</div> 
+      }
+      {address == '' ? 
+      <div className='h-[100%] flex items-center justify-center py-[5px] rounded-[10px]'>
+        Connect Wallet</div> : 
+        <div className='h-[100%] flex items-center justify-center py-[5px] rounded-[10px]'>
+          {address.slice(0,6)}...{address.slice(39)}
+        </div>}
+    </div>
   );
 };
 
-
-const ConnectWalletWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: ${(props) => props.theme.bgDiv};
-  color: ${(props) => props.theme.color};
-  border-radius: 10px;
-  margin-right: 15px;
-  font-family: 'Roboto';
-  font-weight: bold;
-  font-size: small;
-  cursor: pointer;
-`;
-
-const Address = styled.h2`
-    background-color: ${(props) => props.theme.bgSubDiv};
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 0 5px 0 5px;
-    border-radius: 10px;
-`
-
-const Balance = styled.h2`
-    display: flex;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-    margin-right: 5px;
-`
 
 export default Wallet;
