0bfbdba1dca3a36c573d7ada8cd71414bfa3e20a
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-28 01:43:29 +0300

    commit

--- components/Form.js
+++ components/Form.js
@@ -1,18 +1,31 @@
+import React from 'react'
 import styled from 'styled-components';
 import FormRightWrapper from './formRightWrapper'
-import { createContext, useState } from 'react';
+import { createContext, useState, useContext} from 'react';
 import {TailSpin} from 'react-loader-spinner';
 import {ethers} from 'ethers';
 import {toast} from 'react-toastify';
 import DonationTracking from '../artifacts/contracts/DonationTracking.sol/DonationTracking.json'
-import { useContext } from 'react';
+import {create as IPFSHTTPClient} from 'ipfs-http-client';
 
 const FormState = createContext();
+
+const projectId = process.env.NEXT_PUBLIC_IPFS_ID
+const projectSecret = process.env.NEXT_PUBLIC_IPFS_KEY
+const auth = 'Basic ' + Buffer.from(projectId + ":" + projectSecret).toString('base64')
+
+const client = IPFSHTTPClient({
+  host:'ipfs.infura.io',
+  port:5001,
+  protocol: 'https',
+  headers: {
+    authorization: auth
+  }
+})
 
 const Form = () => {
 
 const Handler = useContext(FormState);
-
 
   const [form, setForm] = useState({
     donationEventTitle: "",
@@ -80,6 +93,38 @@ const startDonationEvent = async (e) => {
     }
 }
 
+const [uploadLoading, setUploadLoading] = useState(false);
+ // const [uploaded, setUploaded] = useState(false);
+
+  const uploadFiles = async (e) => {
+    e.preventDefault();
+    setUploadLoading(true);
+
+    if(Handler.form.story !== "") {
+      try {
+        const added = await client.add(Handler.form.story);
+        Handler.setStoryUrl(added.path)
+      } catch (error) {
+        toast.warn(`Error Uploading Story`);
+      }
+    }
+
+
+      if(Handler.image !== null) {
+          try {
+              const added = await client.add(Handler.image);
+              Handler.setImageUrl(added.path)
+          } catch (error) {
+            toast.warn(`Error Uploading Image`);
+          }
+      }
+
+      setUploadLoading(false);
+      setUploaded(true);
+      Handler.setUploaded(true);
+      toast.success("Files Uploaded Sucessfully")
+}
+
 return (
   <FormState.Provider value={{form, setForm, image, setImage, ImageHandler, FormHandler, setImageUrl, setStoryUrl, startDonationEvent, setUploaded}} >
 <FormWrapper>
@@ -112,8 +157,46 @@ return (
                         </div>
                     </div>
                     
-                    <FormRightWrapper />
-                </FormInputsWrapper>               
+                    {/* FormRightWrapper */}
+
+                    <div className='w-[45%]'>
+                        <div className='font flex flex-col mt-[10px]'>
+                            <div className='flex justify-between w-[100%]'>
+                            <div className='flex flex-col w-[45%]'>
+                                <label>Required Amount</label>
+                                <div className='p-[15px] mt-[4px] border-none rounded-[8px] outline-none w-[100%]' onChange={Handler.FormHandler} value={Handler.form.requiredAmount} name="requiredAmount" type={'number'} placeholder='Required Amount'></div>
+                            </div>
+                            <div className='flex flex-col w-[45%]'>
+                                <label>Choose Category</label>
+                                <div className='p-[15px] mt-[4px] border-none rounded-[8px] outline-none w-[100%]' onChange={Handler.FormHandler} value={Handler.form.category} name="category">
+                                <option>Education</option>
+                                <option>Health</option>
+                                <option>Animal</option>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        {/* Image */}
+                        <div className='font flex flex-col mt-[10px]'>
+                            <label>Select Image</label>
+                            <div className='mt-[4px] border-none rounded-[8px] outline-none w-[100%]' alt="dapp" onChange={Handler.ImageHandler} type={'file'} accept='image/*'>
+                            </div>
+                        </div>
+                        {uploadLoading == true ? <div className='button'><TailSpin color='#fff' height={20} /></div> :
+                            uploaded == false ? 
+                            <div className='button' onClick={uploadFiles}>
+                            Upload Files to IPFS
+                            </div>
+                            : <div className='button' style={{cursor: "no-drop"}}>Files uploaded Sucessfully</div>
+                        }
+                        <div className='button' onClick={Handler.startDonationEvent}>
+                            Start DonationEvent
+                        </div>
+                    </div>
+
+
+                </FormInputsWrapper>    
+                           
         }
     </FormMain>
 </FormWrapper>
--- components/FormRightWrapper.js
+++ components/FormRightWrapper.js
@@ -54,128 +54,41 @@ const FormRightWrapper = () => {
       toast.success("Files Uploaded Sucessfully")
 }
   return (
-    <FormRight>
-      <FormInput>
-        <FormRow>
-          <RowFirstInput>
+    <div className='w-[45%]'>
+      <div className='font flex flex-col mt-[10px]'>
+        <div className='flex justify-between w-[100%]'>
+          <div className='flex flex-col w-[45%]'>
             <label>Required Amount</label>
-            <Input onChange={Handler.FormHandler} value={Handler.form.requiredAmount} name="requiredAmount" type={'number'} placeholder='Required Amount'></Input>
-          </RowFirstInput>
-          <RowSecondInput>
+            <div className='p-[15px] mt-[4px] border-none rounded-[8px] outline-none w-[100%]' onChange={Handler.FormHandler} value={Handler.form.requiredAmount} name="requiredAmount" type={'number'} placeholder='Required Amount'></div>
+          </div>
+          <div className='flex flex-col w-[45%]'>
             <label>Choose Category</label>
-            <Select onChange={Handler.FormHandler} value={Handler.form.category} name="category">
+            <div className='p-[15px] mt-[4px] border-none rounded-[8px] outline-none w-[100%]' onChange={Handler.FormHandler} value={Handler.form.category} name="category">
               <option>Education</option>
               <option>Health</option>
               <option>Animal</option>
-            </Select>
-          </RowSecondInput>
-        </FormRow>
-      </FormInput>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* Image */}
-      <FormInput>
+      <div className='font flex flex-col mt-[10px]'>
         <label>Select Image</label>
-        <Image alt="dapp" onChange={Handler.ImageHandler} type={'file'} accept='image/*'>
-        </Image>
-      </FormInput>
-      {uploadLoading == true ? <Button><TailSpin color='#fff' height={20} /></Button> :
+        <div className='mt-[4px] border-none rounded-[8px] outline-none w-[100%]' alt="dapp" onChange={Handler.ImageHandler} type={'file'} accept='image/*'>
+        </div>
+      </div>
+      {uploadLoading == true ? <div className='button'><TailSpin color='#fff' height={20} /></div> :
         uploaded == false ? 
-        <Button onClick={uploadFiles}>
+        <div className='button' onClick={uploadFiles}>
           Upload Files to IPFS
-        </Button>
-        : <Button style={{cursor: "no-drop"}}>Files uploaded Sucessfully</Button>
+        </div>
+        : <div className='button' style={{cursor: "no-drop"}}>Files uploaded Sucessfully</div>
       }
-      <Button onClick={Handler.startDonationEvent}>
+      <div className='button' onClick={Handler.startDonationEvent}>
         Start DonationEvent
-      </Button>
-    </FormRight>
+      </div>
+    </div>
   )
 }
-const FormRight = styled.div`
-  width:45%;
-`
-
-const FormInput = styled.div`
-  display:flex ;
-  flex-direction:column;
-  font-family:'poppins';
-  margin-top:10px ;
-`
-
-const FormRow = styled.div`
-  display: flex;
-  justify-content:space-between;
-  width:100% ;
-`
-
-const Input = styled.input`
-  padding:15px;
-  background-color:${(props) => props.theme.bgDiv} ;
-  color:${(props) => props.theme.color} ;
-  margin-top:4px;
-  border:none ;
-  border-radius:8px ;
-  outline:none;
-  font-size:large;
-  width:100% ;
-` 
-
-const RowFirstInput = styled.div`
-  display:flex ;
-  flex-direction:column ;
-  width:45% ;
-`
-
-const RowSecondInput = styled.div`
-  display:flex ;
-  flex-direction:column ;
-  width:45% ;
-`
-
-const Select = styled.select`
-  padding:15px;
-  background-color:${(props) => props.theme.bgDiv} ;
-  color:${(props) => props.theme.color} ;
-  margin-top:4px;
-  border:none ;
-  border-radius:8px ;
-  outline:none;
-  font-size:large;
-  width:100% ;
-`
-
-const Image = styled.input`
-  background-color:${(props) => props.theme.bgDiv} ;
-  color:${(props) => props.theme.color} ;
-  margin-top:4px;
-  border:none ;
-  border-radius:8px ;
-  outline:none;
-  font-size:large;
-  width:100% ;
-  &::-webkit-file-upload-button {
-    padding: 15px ;
-    background-color: ${(props) => props.theme.bgSubDiv} ;
-    color: ${(props) => props.theme.color} ;
-    outline:none ;
-    border:none ;
-    font-weight:bold ;
-  }  
-`
-
-const Button = styled.button`
-  display: flex;
-  justify-content:center;
-  width:100% ;
-  padding:15px ;
-  color:white ;
-  background-color:#00b712 ;
-  background-image:
-      linear-gradient(180deg, #00b712 0%, #5aff15 80%) ;
-  border:none;
-  margin-top:30px ;
-  cursor: pointer;
-  font-weight:bold ;
-  font-size:large ;
-`
 
 export default FormRightWrapper
