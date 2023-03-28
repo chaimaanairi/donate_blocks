43f3084947f5bcda7a5335b85ff53f2eb1c872a5
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-28 16:01:16 +0300

    commit

--- components/web3/walletbar.js
+++ components/web3/walletbar.js
@@ -1,10 +1,25 @@
   /* eslint-disable jsx-a11y/alt-text */
-//import {useAccount,useNetwork} from "../../hooks/useHooks";
+  import { useState } from "react";
 
 import React from "react";
 
+const networks = {
+  polygon: {
+    chainId: `0x${Number(80001).toString(16)}`,
+    chainName: "Polygon Testnet",
+    nativeCurrency: {
+      name: "MATIC",
+      symbol: "MATIC",
+      decimals: 18,
+    },
+    rpcUrls: ["https://rpc-mumbai.maticvigil.com/"],
+    blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+  },
+};
+
+
 export default function WalletBar() {
-  //const { account } = useAccount();
+  const [address, setAddress] = useState("");
  // const { network } = useNetwork();
 
  return (
@@ -15,7 +30,14 @@ export default function WalletBar() {
   </div>  
 
   <div className="mb-5">        
-    <h2 className="text-2xl pb-4">Hello, account.data</h2>
+    <h2 className="text-2xl pb-4">Hello, 
+    {address == '' ? 
+      <div className=''></div> : 
+        <div className='text-black'>
+          {address.slice(0,6)}...{address.slice(39)} 
+        </div>
+        }
+    </h2>
     <h2 className="subtitle  text-xl mb-2">I hope you are having a great day!</h2>
     <p className="text-black ">
 
@@ -38,7 +60,7 @@ export default function WalletBar() {
             
             <span>Currently on </span>
             <strong className="text-2xl"> 
-               network.data
+            {networks["polygon"].chainName}
             </strong>
 
           </div>
