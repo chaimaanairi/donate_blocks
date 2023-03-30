38b5fabbea549dc9dda29c8b0eab8f0301cc31c1
ChaimaaN <chaimaanairi91@gmail.com>
2023-03-31 00:35:38 +0300

    commit

--- components/web3/walletNavbar.tsx
+++ components/web3/walletNavbar.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-
 import { Menu } from "@headlessui/react";
 import Link from "next/link";
 import React from "react";
--- contracts/DonationTracking.sol
+++ contracts/DonationTracking.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Unlicensed
-
+ 
 pragma solidity >0.7.0 <=0.9.0;
 
 contract DonationTracking {
